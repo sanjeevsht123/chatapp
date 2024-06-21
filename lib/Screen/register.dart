0a5d4e71faebbea4f27f71ca62b9d79e2413947e
Sanjeev Shrestha <sanjeevsht123@gmail.com>
2024-06-21 15:37:07 +0545

    added splash screen,app icon and changed app name

--- lib/Screen/register.dart
+++ lib/Screen/register.dart
@@ -42,79 +42,81 @@ class Register extends StatelessWidget {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //logo
-            Icon(
-              Icons.message_rounded,
-              size: 85,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            //you have been missed message
-            Text(
-              "Let's create account for you",
-              style: TextStyle(color: Theme.of(context).colorScheme.primary),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-
-            //textfield for username and password
-            Mytextfield(
-                controller: emailController,
-                hintText: "Email",
-                obsecure: false),
-            const SizedBox(
-              height: 10,
-            ),
-            Mytextfield(
-                controller: passwordController,
-                hintText: "Password",
-                obsecure: true),
-            //button to login
-            const SizedBox(
-              height: 15,
-            ),
-            Mytextfield(
-                controller: confirmController,
-                hintText: "conform Password",
-                obsecure: true),
-            //button to login
-            const SizedBox(
-              height: 15,
-            ),
-            Mybotton(
-              bottonText: "Sign up",
-              onTap: () => signup(context),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            //Text to register page:Not a member ?  Register Now.
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    "Already have an account ?",
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.primary,
-                    ),
-                  ),
-                  TextButton(
-                      onPressed: onTap,
-                      child: Text("Login",
-                          style: TextStyle(
-                            color: Colors.blue,
-                          )))
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              Icon(
+                Icons.message_rounded,
+                size: 85,
               ),
-            )
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              //you have been missed message
+              Text(
+                "Let's create account for you",
+                style: TextStyle(color: Theme.of(context).colorScheme.primary),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+
+              //textfield for username and password
+              Mytextfield(
+                  controller: emailController,
+                  hintText: "Email",
+                  obsecure: false),
+              const SizedBox(
+                height: 10,
+              ),
+              Mytextfield(
+                  controller: passwordController,
+                  hintText: "Password",
+                  obsecure: true),
+              //button to login
+              const SizedBox(
+                height: 15,
+              ),
+              Mytextfield(
+                  controller: confirmController,
+                  hintText: "conform Password",
+                  obsecure: true),
+              //button to login
+              const SizedBox(
+                height: 15,
+              ),
+              Mybotton(
+                bottonText: "Sign up",
+                onTap: () => signup(context),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              //Text to register page:Not a member ?  Register Now.
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Already have an account ?",
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                    TextButton(
+                        onPressed: onTap,
+                        child: Text("Login",
+                            style: TextStyle(
+                              color: Colors.blue,
+                            )))
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
